d637c6b588e6bd50de73db1f6a8f987c4c41c6a4
Axel José Mosquito <112713383+AxlRoose1702@users.noreply.github.com>
2024-03-12 11:17:54 -0600

    Se agrega validador para creacion de un examen

---
+++ src/PROYECTOCLINICAL.Application.UseCase/UseCase/Exam/Commands/CreateCommand/CreateExamValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace PROYECTOCLINICAL.Application.UseCase.UseCase.Exam.Commands.CreateCommand
+{
+    public class CreateExamValidator : AbstractValidator<CreateExamCommand>
+    {
+        public CreateExamValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull().WithMessage("El campo nombre no puede estar nulo")
+                .NotEmpty().WithMessage("El campo nombre no puede estar vacio");
+        }
+    }
+}
